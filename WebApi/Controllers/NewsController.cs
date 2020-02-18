0e8de9b3716e30f1b33b585a9d1cff3fa8316913
vvoskanyan <vvoskanyan@vmware.com>
2020-02-18 16:20:07 +0400

    some chanegs

--- WebApi/Controllers/NewsController.cs
+++ WebApi/Controllers/NewsController.cs
@@ -3,44 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using WebApi.Database;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/news")]
+    [ApiController]
     public class NewsController : Controller
     {
-        // GET: api/values
+        public DbManager DbManager { get; }
+
+        public NewsController(DbManager dbManager)
+        {
+            DbManager = dbManager;
+        }
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            var news = DbManager.GetAnnouncements();
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(news);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
+
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
+        // DELETE api/news/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+
         }
     }
 }
