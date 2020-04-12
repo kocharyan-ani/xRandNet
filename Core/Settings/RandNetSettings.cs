aa11c74cf8c88e63e121ba2860a9e6046c863671
vvoskanyan <vvoskanyan@vmware.com>
2020-04-12 19:57:11 +0400

    Paths changed

--- Core/Settings/RandNetSettings.cs
+++ Core/Settings/RandNetSettings.cs
@@ -7,19 +7,17 @@ using Core.Exceptions;
 using Core.Enumerations;
 using Core.Utility;
 
-namespace Core.Settings
-{
+namespace Core.Settings {
     /// <summary>
     /// RandNet application settings organization and manipulation interface.
     /// </summary>
-    public static class RandNetSettings
-    {
-        static private String defaultDirectory = 
-            Directory.GetParent(Directory.GetCurrentDirectory()).FullName + "\\xRandNet";
+    public static class RandNetSettings {
+        static private String defaultDirectory =
+            Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "xRandNet");
 
         static private Configuration config;
 
-        static private String loggingDirectory = "";        
+        static private String loggingDirectory = "";
         static private String storageDirectory = "";
         static private String tracingDirectory = "";
         static private TracingType tracingType;
@@ -29,22 +27,15 @@ namespace Core.Settings
         static private String modelCheckingToolDirectory = "";
         static private String dataConvertionToolDirectory = "";
 
-        static RandNetSettings()
-        {
-            if (!CustomLogger.WebMode)
-            {
+        static RandNetSettings() {
+            if (!CustomLogger.WebMode) {
                 SetConfiguration();
             }
         }
 
-        static public String LoggingDirectory
-        {
-            get
-            {
-                return (loggingDirectory == "") ? defaultDirectory + "\\Logging" : loggingDirectory;
-            }
-            set
-            {
+        static public String LoggingDirectory {
+            get { return (loggingDirectory == "") ? Path.Combine(defaultDirectory, "Logging") : loggingDirectory; }
+            set {
                 if (value == loggingDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -54,20 +45,15 @@ namespace Core.Settings
 
                 if (Directory.Exists(loggingDirectory) == false)
                     Directory.CreateDirectory(loggingDirectory);
-                
+
                 config.AppSettings.Settings["LoggingDirectory"].Value = loggingDirectory;
                 InitializeLogging("");
             }
         }
 
-        static public String StorageDirectory
-        {
-            get
-            {
-                return (storageDirectory == "") ? defaultDirectory + "\\Results" : storageDirectory;
-            }
-            set
-            {
+        static public String StorageDirectory {
+            get { return (storageDirectory == "") ? Path.Combine(defaultDirectory, "Results") : storageDirectory; }
+            set {
                 if (value == storageDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -82,14 +68,9 @@ namespace Core.Settings
             }
         }
 
-        static public String TracingDirectory
-        {
-            get
-            {
-                return (tracingDirectory == "") ? defaultDirectory + "\\Tracing" : tracingDirectory;
-            }
-            set
-            {
+        static public String TracingDirectory {
+            get { return (tracingDirectory == "") ? Path.Combine(defaultDirectory, "Tracing") : tracingDirectory; }
+            set {
                 if (value == tracingDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -104,14 +85,9 @@ namespace Core.Settings
             }
         }
 
-        static public TracingType TracingType
-        {
-            get
-            {
-                return tracingType;
-            }
-            set
-            {
+        static public TracingType TracingType {
+            get { return tracingType; }
+            set {
                 if (value == tracingType)
                     return;
                 tracingType = value;
@@ -119,14 +95,9 @@ namespace Core.Settings
             }
         }
 
-        static public ManagerType WorkingMode 
-        {
-            get
-            {
-                return workingMode;
-            }
-            set
-            {
+        static public ManagerType WorkingMode {
+            get { return workingMode; }
+            set {
                 if (value == workingMode)
                     return;
                 workingMode = value;
@@ -134,14 +105,13 @@ namespace Core.Settings
             }
         }
 
-        static public String StaticGenerationDirectory
-        {
-            get
-            {
-                return (staticGenerationDirectory == "") ? defaultDirectory + "\\Tracing" : staticGenerationDirectory;
+        static public String StaticGenerationDirectory {
+            get {
+                return (staticGenerationDirectory == "")
+                    ? Path.Combine(defaultDirectory, "Tracing")
+                    : staticGenerationDirectory;
             }
-            set
-            {
+            set {
                 if (value == staticGenerationDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -156,14 +126,13 @@ namespace Core.Settings
             }
         }
 
-        static public String MatrixConvertionToolDirectory
-        {
-            get
-            {
-                return (matrixConvertionToolDirectory == "") ? defaultDirectory + "\\Tracing" : matrixConvertionToolDirectory;
+        static public String MatrixConvertionToolDirectory {
+            get {
+                return (matrixConvertionToolDirectory == "")
+                    ? Path.Combine(defaultDirectory, "Tracing")
+                    : matrixConvertionToolDirectory;
             }
-            set
-            {
+            set {
                 if (value == matrixConvertionToolDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -178,14 +147,13 @@ namespace Core.Settings
             }
         }
 
-        static public String ModelCheckingToolDirectory
-        {
-            get
-            {
-                return (modelCheckingToolDirectory == "") ? defaultDirectory + "\\Tracing" : modelCheckingToolDirectory;
+        static public String ModelCheckingToolDirectory {
+            get {
+                return (modelCheckingToolDirectory == "")
+                    ? Path.Combine(defaultDirectory, "Tracing")
+                    : modelCheckingToolDirectory;
             }
-            set
-            {
+            set {
                 if (value == modelCheckingToolDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -200,14 +168,13 @@ namespace Core.Settings
             }
         }
 
-        static public String DataConvertionToolDirectory
-        {
-            get
-            {
-                return (dataConvertionToolDirectory == "") ? defaultDirectory + "\\Results" : dataConvertionToolDirectory;
+        static public String DataConvertionToolDirectory {
+            get {
+                return (dataConvertionToolDirectory == "")
+                    ? Path.Combine(defaultDirectory, "Results")
+                    : dataConvertionToolDirectory;
             }
-            set
-            {
+            set {
                 if (value == dataConvertionToolDirectory)
                     return;
                 if (value.EndsWith(Path.DirectorySeparatorChar.ToString()))
@@ -225,30 +192,26 @@ namespace Core.Settings
         /// <summary>
         /// Refreshes app.config file content.
         /// </summary>
-        static public void Refresh()
-        {
+        static public void Refresh() {
             config.Save(ConfigurationSaveMode.Modified);
             ConfigurationManager.RefreshSection("appSettings");
             ConfigurationManager.RefreshSection("loggingConfiguration");
         }
 
-        static public void InitializeLogging(String fileName)
-        {
-            LoggingSettings loggingSettings = (LoggingSettings)config.GetSection("loggingConfiguration");
+        static public void InitializeLogging(String fileName) {
+            LoggingSettings loggingSettings = (LoggingSettings) config.GetSection("loggingConfiguration");
             Debug.Assert(loggingSettings != null);
             TraceListenerData traceListenerData = loggingSettings.TraceListeners.Get("Flat File Trace Listener");
             Debug.Assert(traceListenerData != null);
             FlatFileTraceListenerData file = traceListenerData as FlatFileTraceListenerData;
             Debug.Assert(file != null);
 
-            if (fileName == "")
-            {
+            if (fileName == "") {
                 File.Copy(file.FileName, LoggingDirectory + Path.GetFileName(file.FileName));
                 file.FileName = LoggingDirectory + Path.GetFileName(file.FileName);
             }
-            else
-            {
-                file.FileName = LoggingDirectory + "\\" + fileName + ".log";
+            else {
+                file.FileName = Path.Combine(LoggingDirectory, fileName + ".log");
                 loggingDirectory = Path.GetDirectoryName(file.FileName);
             }
 
@@ -256,31 +219,28 @@ namespace Core.Settings
             ConfigurationManager.RefreshSection("loggingConfiguration");
         }
 
-        static private void RefreshLoggingPath()
-        {
+        static private void RefreshLoggingPath() {
             InitializeLogging("");
         }
 
         private static void SetConfiguration()
         {
             config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-                
-            try
-            {
+
+            try {
                 loggingDirectory = config.AppSettings.Settings["LoggingDirectory"].Value;
                 storageDirectory = config.AppSettings.Settings["StorageDirectory"].Value;
                 tracingDirectory = config.AppSettings.Settings["TracingDirectory"].Value;
-                tracingType = (TracingType)Enum.Parse(typeof(TracingType),
+                tracingType = (TracingType) Enum.Parse(typeof(TracingType),
                     config.AppSettings.Settings["Tracingtype"].Value);
-                workingMode = (ManagerType)Enum.Parse(typeof(ManagerType), 
+                workingMode = (ManagerType) Enum.Parse(typeof(ManagerType),
                     config.AppSettings.Settings["WorkingMode"].Value);
                 staticGenerationDirectory = config.AppSettings.Settings["StaticGenerationDirectory"].Value;
                 matrixConvertionToolDirectory = config.AppSettings.Settings["MatrixConvertionToolDirectory"].Value;
                 modelCheckingToolDirectory = config.AppSettings.Settings["ModelCheckingToolDirectory"].Value;
                 dataConvertionToolDirectory = config.AppSettings.Settings["DataConvertionToolDirectory"].Value;
             }
-            catch
-            {
+            catch {
                 throw new CoreException("The structure of Configuration file is not correct.");
             }
         }
